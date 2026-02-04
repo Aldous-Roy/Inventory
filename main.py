50749f433e53c871cad5000e7c31928cda6b6535
Aldous Roy <aldousroy007@gmail.com>
2026-02-04 23:15:22 +0530

    Implement database setup and product model for FastAPI application

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+import database_models
 from models import product
+from database import session,engine
 app=FastAPI()
+
+database_models.base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def greet():
@@ -11,9 +15,20 @@ products=[
     product(id=2, name="Smartphone", description="A latest model smartphone", price=599.99, quantity=20),
     product(id=3, name="Headphones", description="Noise-cancelling headphones", price=199.99, quantity=15) 
 ]
+
+def inti_db():
+    db=session()
+    count =db.query(database_models.Product).count()
+    if count==0:
+        for prod in products:
+            db.add(database_models.Product(**prod.model_dump()))
+        db.commit()
+inti_db()
 @app.get("/products")
 def get_products():
-    return {"products": products}
+    db=session()
+    db_products=db.query(database_models.Product).all()
+    return {"products": db_products}
 
 @app.get("/products/{id}")
 def get_Product_by_id(id:int):
